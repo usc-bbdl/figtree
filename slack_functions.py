a8866d1aff5b4b5cad56e8df5732834c3218c496
Brian Cohn <brian.cohn@usc.edu>
2019-09-19 16:28:04 -0700

    fixed bool to string issue

--- slack_functions.py
+++ slack_functions.py
@@ -18,13 +18,13 @@ def slack_confirmed_receipt(response):
 def remind_lab_to_upload():
     data = '{"text":"Please upload your weekly update figure here: \n %s"}' % (DRIVE_QUEUE)
     response = requests.post(SLACK_API, headers=headers, data=data)
-    print("WebHook Successful: " + slack_confirmed_receipt(response))
+    print("WebHook Successful: " + str(slack_confirmed_receipt(response)))
 
 
 def distribute_link_to_lab():
     data = '{"text":"Lab Meeting PPT for Week  %s: \n %s"}' % (week_number(), DRIVE_FOLDER)
     response = requests.post(SLACK_API, headers=headers, data=data)
-    print("WebHook Successful: " + slack_confirmed_receipt(response))
+    print("WebHook Successful: " + str(slack_confirmed_receipt(response)))
 
 
 def week_number():
