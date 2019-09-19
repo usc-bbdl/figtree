e697bbdba09013c0dce23c973161b553a418712c
Brian Cohn <brian.cohn@usc.edu>
2019-09-19 16:17:56 -0700

    printing sample responses

--- slack_functions.py
+++ slack_functions.py
@@ -11,10 +11,12 @@ headers = {
 def remind_lab_to_upload():
     data = '{"text":"Please upload your weekly update figure here: \n %s"}'%(DRIVE_QUEUE)
     response = requests.post(SLACK_API, headers=headers, data=data)
+    print response
 
 def distribute_link_to_lab():
     data = '{"text":"Lab Meeting PPT for Week  %s: \n %s"}'%(week_number(),DRIVE_FOLDER)
     response = requests.post(SLACK_API, headers=headers, data=data)
+    print response
 
 def week_number():
     return datetime.date(2010, 6, 16).isocalendar()[1]
--- upload_to_gdrive.py
+++ upload_to_gdrive.py
@@ -10,8 +10,8 @@ import sys
 
 assert os.path.exists('temp_output.txt'), "'temp_output.txt' does not exist. Need to run 'build_weekly_ppt.py' before " \
                                           "'upload_to_gdrive.py'. "
-
-
+distribute_link_to_lab()
+remind_lab_to_upload()
 
 def etl_build_details():
     tempDict = {}
